927aa16cf65b531369797a52d311c7e913f98e83
indx0 <111313725+indxx0@users.noreply.github.com>
2026-06-07 18:40:01 +0300

    Refactor Node constructor to use explicit keyword

--- include/bst.h
+++ include/bst.h
@@ -6,16 +6,16 @@
 
 template <typename T>
 class BST {
-public:
+ public:
     struct Node {
         T value;
         int count;
         Node* left;
         Node* right;
-        Node(T val) : value(val), count(1), left(nullptr), right(nullptr) {}
+        explicit Node(T val) : value(val), count(1), left(nullptr), right(nullptr) {}
     };
 
-private:
+ private:
     Node* root;
 
     void destroy(Node* node) {
@@ -40,9 +40,9 @@ private:
         inOrderCollect(node->right, vec);
     }
 
-public:
+ public:
     BST() : root(nullptr) {}
-    
+
     ~BST() {
         destroy(root);
     }
